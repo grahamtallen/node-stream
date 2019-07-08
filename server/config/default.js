7cf593e4f83004ff8a3bc3764494e7ba37e21132
Graham Allen <grahamtallen94@gmail.com>
2019-07-08 00:55:37 -0500

    audio only playable in vlc

--- server/config/default.js
+++ server/config/default.js
@@ -17,14 +17,14 @@ const config = {
             allow_origin: '*'
         },
         trans: {
-            ffmpeg: '/usr/bin/ffmpeg',
+            ffmpeg: '/usr/local/bin/ffmpeg',
             tasks: [
                 {
                     app: 'live',
-                    hls: true,
-                    hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
-                    dash: true,
-                    dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+                    ac: "aac",
+                    acParam: ['-ab', '64k', '-ac', '1', '-ar', '44100'],
+                    rtmp:true,
+                    rtmpApp:'live2',
                 }
             ]
         }
